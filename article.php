b96b899c5b71b7723a08e71bc9020ee366e23832
gio2396ch <g.chkhik@gmail.com>
2019-09-10 16:24:54 -0400

    Update article.php

--- article.php
+++ article.php
@@ -2,7 +2,7 @@
 <head>
 <meta name = "keywords" content = "Blog Keywords">
 <meta name = "description" content = "Blog">
-<meta name = "author" content= "some nigga">
+<meta name = "author" content= "George Chkhikvadze">
 <meta name = "viewport" content = "width=device-width, initial-scale =1.0">
 <link rel = "stylesheet" type="text/css" href="style.css">
 </head>
